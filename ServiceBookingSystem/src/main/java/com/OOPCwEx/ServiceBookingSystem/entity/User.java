c42876148ba3f7eb5878c8c143ec1419a05fb2b4
Monali Suriarachchi <monalisuriarachchi@gmail.com>
2024-12-07 20:40:28 +0530

    feat: Add User entity with database mapping and fields

--- ServiceBookingSystem/src/main/java/com/OOPCwEx/ServiceBookingSystem/entity/User.java
+++ ServiceBookingSystem/src/main/java/com/OOPCwEx/ServiceBookingSystem/entity/User.java
@@ -1,4 +1,28 @@
 package com.OOPCwEx.ServiceBookingSystem.entity;
 
+import com.OOPCwEx.ServiceBookingSystem.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "Users")
+@Data
+
 public class User {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String email;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phoneNumber;
+
+    private UserRole role;
 }
