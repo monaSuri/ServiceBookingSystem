331af6bff83e3dcb98264d4d03c36a71835b42eb
Monali Suriarachchi <monalisuriarachchi@gmail.com>
2024-12-07 18:27:21 +0530

    Imported iogin and signup component

--- ServiceBookingSystemWeb/src/app/app.module.ts
+++ ServiceBookingSystemWeb/src/app/app.module.ts
@@ -1,3 +1,40 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-export const routes: Routes = [];
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './basic/components/login/login.component';
+import { SignupComponent } from './basic/components/signup/signup.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { DemoNgZorroAntdModule } from './DemoNgZorroAntdModule';
+
+registerLocaleData(en);
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    DemoNgZorroAntdModule,
+    ReactiveFormsModule
+
+  ],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US },
+    provideAnimationsAsync(),
+    provideHttpClient()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
