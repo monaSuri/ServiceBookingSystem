38dbaf257b21e077b75dfdae7e7cda9351b451b2
Monali Suriarachchi <monalisuriarachchi@gmail.com>
2024-12-08 20:36:56 +0530

    Update auth.service.ts

--- ServiceBookingSystemWeb/src/app/basic/services/auth/auth.service.ts
+++ ServiceBookingSystemWeb/src/app/basic/services/auth/auth.service.ts
@@ -1,22 +1,20 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const BASIC_URL = 'http://localhost:8090/'
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:8090/api';
+  private baseUrl = 'http://localhost:8090/api/auth';
 
-  constructor(private http: HttpClient,) { }
+  constructor(private http: HttpClient) {}
 
-  registerClient(data: any) {
-    return this.http.post(`${this.apiUrl}/register`, data);
+  registerClient(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/client/sign-up`, data);
   }
 
-  registerCompany(signupRequestDTO:any): Observable<any>{
-    return this.http.post(BASIC_URL + "company/sign-up", signupRequestDTO);
+  registerCompany(data: any): Observable<any> {
+    return this.http.post(`${this.baseUrl}/company/sign-up`, data);
   }
 }
