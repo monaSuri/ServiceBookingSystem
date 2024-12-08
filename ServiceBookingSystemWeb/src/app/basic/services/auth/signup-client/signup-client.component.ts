f67684000e794abd76ad89cbacbd83f074fd035e
Monali Suriarachchi <monalisuriarachchi@gmail.com>
2024-12-08 10:47:21 +0530

    Create sign up client component

---
+++ ServiceBookingSystemWeb/src/app/basic/services/auth/signup-client/signup-client.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-client',
+  standalone: true,
+  imports: [],
+  templateUrl: './signup-client.component.html',
+  styleUrl: './signup-client.component.scss'
+})
+export class SignupClientComponent {
+
+}
