98b2e18a258ac0508841ed8476e600d3ec07e765
Monali Suriarachchi <monalisuriarachchi@gmail.com>
2024-12-08 22:52:41 +0530

    Updated SimpleCors.Filter.java

--- ServiceBookingSystem/src/main/java/com/OOPCwEx/ServiceBookingSystem/configs/SimpleCorsFilter.java
+++ ServiceBookingSystem/src/main/java/com/OOPCwEx/ServiceBookingSystem/configs/SimpleCorsFilter.java
@@ -1,28 +1,29 @@
 package com.OOPCwEx.ServiceBookingSystem.configs;
 
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
+import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class SimpleCorsFilter implements Filter {
 
     @Override
-    public void doFilter(jakarta.servlet.ServletRequest req, jakarta.servlet.ServletResponse res, FilterChain chain)
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
             throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
-
+        Map<String, String> map = new HashMap<>();
+        String originHeader = request.getHeader("Origin");
         // Add CORS headers
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+        response.setHeader("Access-Control-Allow-Origin",originHeader);
         response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, X-Requested-With");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "*");
 
         // Handle preflight (OPTIONS) requests
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
